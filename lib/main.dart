339aed39a2e0c0dd295b160f57e46f6e1c57f420
MuhammadAhmerMehfooz <ahmermehfooz536@gmail.com>
2025-01-17 11:41:15 +0500

    Your message about the changes

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CallExecution(),
+      home: WorkPlan(),
     );
   }
 }
